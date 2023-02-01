fd545437fb6c1257edb34a92bbe554d86e92835d
Travis Stodter <tstodter@gmail.com>
2023-02-01 11:54:12 -0500

    Some messing with delayed elements

--- index.js
+++ index.js
@@ -1,42 +1,98 @@
-const htl = window.htl;
-const svg = htl.svg.fragment;
+// const htl = window.htl;
+// const svg = htl.svg.fragment;
+const makeElement = (html) => {
+  let template = document.createElement('template');
+  template.innerHTML = html.trim();
 
-const truchetCurve = () => svg`
+  return template.content.firstChild;
+};
+
+const truchetCurve = `
   <path d="M 0.5 1 Q 0.5 0.5, 1 0.5" fill="none" stroke="black" stroke-width=".01"/>
 `;
 
 const translate = (vector, elem) => {
-  return svg`<g transform="translate(${vector[0]} ${vector[1]})">
-    ${elem.cloneNode(true)}                   
+  return `<g transform="translate(${vector[0]} ${vector[1]})">
+    ${elem}                   
   </g>`;
 };
 
 const rotate = (degrees, aboutPoint, elem) => {
-  return svg`<g transform="rotate(${degrees} ${aboutPoint[0]} ${aboutPoint[1]})">
-    ${elem.cloneNode(true)}
+  return `<g transform="rotate(${degrees} ${aboutPoint[0]} ${aboutPoint[1]})">
+    ${elem}
   </g>`
 };
 
 const reflect = (vector, elem) => {
-  return svg`<g transform="translate(${vector[0]} ${vector[1]}) 
+  return `<g transform="translate(${vector[0]} ${vector[1]}) 
                            scale(-1, 1) 
                            translate(${-vector[0]} ${-vector[1]})">
-    ${elem.cloneNode(true)}                   
+    ${elem}                   
   </g>`;
 };
 
-const rotated = rotate(0, [0.5, 0.5], truchetCurve());
+
+class Tile {
+  constructor(svgStringF) {
+    this.svgStringF = svgStringF;
+  }
+
+  translate(vector) {
+    if (typeof vector === 'function')
+      return new Tile(() => translate(vector(), this.svgStringF()))
+    return new Tile(() => translate(vector, this.svgStringF()));
+  }
+
+  rotate(degrees, aboutPoint) {
+    if (typeof degrees === 'function')
+      return new Tile(() => rotate(degrees(), aboutPoint, this.svgStringF()))
+    return new Tile(() => rotate(degrees, aboutPoint, this.svgStringF()));
+  }
+
+  reflect(vector) {
+    return new Tile(() => reflect(vector, this.svgStringF()));
+  }
+
+  multiply(diffVector, num) {
+    return new Tile(() => {
+      let row = '';
+      let currentVec = [0, 0];
+      for (let i = 0; i < num; i++) {
+        row += translate(currentVec, this.svgStringF());
+        currentVec = [currentVec[0] + diffVector[0], currentVec[1] + diffVector[1]];
+      }
+      return row;
+    });
+  }
+
+  toString() {
+    return this.svgStringF();
+  }
+
+  toElement() {
+    return makeElement(this.toString());
+  }
+}
+
+const rotated = rotate(0, [0.5, 0.5], truchetCurve);
 const reflected = reflect([0.75, 0.75], rotated);
 const translated = translate([-0.5, -0.5], reflected);
 
-const finalTile = translated;
+const finalTile = new Tile(() => truchetCurve)
+  .rotate(() => Math.random() * 30, [0.5, 0.5])
+  // .reflect([0.75, 0.75])
+  // .translate([-2, -0.5])
+  .multiply([1, 0], 20)
+  .multiply([0, 1], 20)
+  .toString();
 
 const theSVGElement = document.querySelector('#svg_container');
 theSVGElement.append(
-  htl.html`
-    <svg width="500px" height="500px" viewBox="-2 -2 4 4">
-      ${truchetCurve()}
-      ${svg`<circle cx="0" cy="0" r="0.05" fill="red" />`}
+  makeElement(`
+    <svg width="500px" height="500px" viewBox="0 0 10 10">
+      ${truchetCurve.trim()}
+      <circle cx="0" cy="0" r="0.05" fill="red" />
       ${finalTile}
-    </svg>`
-)
+    </svg>
+  `)
+);
